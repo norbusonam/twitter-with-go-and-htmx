712f9d7636f573baf361d2dc28a507629d587853
norbusonam <nsonam1822@gmail.com>
2023-12-13 17:41:09 -0500

    route placeholders

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
@@ -10,7 +11,30 @@ func main() {
 	e := echo.New()
 
 	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello, World!")
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	})
+
+	e.GET("/home", func(c echo.Context) error {
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	})
+
+	e.GET("/explore", func(c echo.Context) error {
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	})
+
+	e.GET("/bookmarks", func(c echo.Context) error {
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	})
+
+	e.GET("/:username", func(c echo.Context) error {
+		username := c.Param("username")
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented (username=%s)", c.Path(), username))
+	})
+
+	e.GET("/:username/:postId", func(c echo.Context) error {
+		username := c.Param("username")
+		postId := c.Param("postId")
+		return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented (username=%s, postId=%s)", c.Path(), username, postId))
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
