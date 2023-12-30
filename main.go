c026bf14256d38ea01acdadd576d649b075196cc
norbusonam <nsonam1822@gmail.com>
2023-12-30 18:54:15 -0500

    logging middleware

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/norbusonam/twitter-with-go-and-htmx/pkg/db"
 	"github.com/norbusonam/twitter-with-go-and-htmx/pkg/handlers"
 )
 
 func main() {
 	e := echo.New()
+
+	// +--------------------+
+	// | Logging Middleware |
+	// +--------------------+
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		BeforeNextFunc: func(c echo.Context) {
+			c.Set("customValueFromContext", 42)
+		},
+		LogValuesFunc: func(c echo.Context, v middleware.RequestLoggerValues) error {
+			value, _ := c.Get("customValueFromContext").(int)
+			fmt.Printf("REQUEST: uri: %v, status: %v, custom-value: %v\n", v.URI, v.Status, value)
+			return nil
+		},
+	}))
 
 	// +--------------+
 	// | Static Files |
