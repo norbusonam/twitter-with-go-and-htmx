fc575bb99daf0a9bda224f768a7d8ee8d60fbd55
norbusonam <nsonam1822@gmail.com>
2024-01-01 19:45:54 -0500

    transient auth handlers

--- pkg/handlers/auth.go
+++ pkg/handlers/auth.go
@@ -3,16 +3,43 @@ package handlers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/labstack/echo/v4"
+	"github.com/norbusonam/twitter-with-go-and-htmx/pkg/db"
 )
 
 func SignIn(c echo.Context) error {
-	return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	user, err := db.GetUserByUsername(username)
+	if err != nil {
+		return err
+	}
+	if user.Password != password {
+		return fmt.Errorf("invalid password")
+	}
+	c.SetCookie(&http.Cookie{
+		Name:  "user_id",
+		Value: strconv.Itoa(user.ID),
+	})
+	return c.Redirect(http.StatusFound, "/home")
 }
 
 func SignUp(c echo.Context) error {
-	return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented", c.Path()))
+	username := c.FormValue("username")
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+	fmt.Println(username, email, password)
+	user, err := db.CreateUser(username, email, password)
+	if err != nil {
+		return err
+	}
+	c.SetCookie(&http.Cookie{
+		Name:  "user_id",
+		Value: strconv.Itoa(user.ID),
+	})
+	return c.Redirect(http.StatusFound, "/home")
 }
 
 func SignOut(c echo.Context) error {
