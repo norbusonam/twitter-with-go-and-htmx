d4e7197daf5a05f7a016072ba3eb7d025f64496c
norbusonam <nsonam1822@gmail.com>
2024-01-07 09:29:00 -0800

    render user page

--- pkg/handlers/user.go
+++ pkg/handlers/user.go
@@ -5,12 +5,23 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/norbusonam/twitter-with-go-and-htmx/pkg/db"
+	"github.com/norbusonam/twitter-with-go-and-htmx/pkg/templates"
 )
 
 func UserPage(c echo.Context) error {
 	// render user page
 	username := c.Param("username")
-	return c.String(http.StatusNotImplemented, fmt.Sprintf("%s not implemented (username=%s)", c.Path(), username))
+	user, err := db.GetUserByUsername(username)
+	if err != nil {
+		return err
+	}
+	authenticatedUser, err := getAuthenticatedUser(c.Cookies())
+	if err != nil {
+		return err
+	}
+	templates.User(user, authenticatedUser).Render(c.Request().Context(), c.Response().Writer)
+	return nil
 }
 
 func UserLikesPage(c echo.Context) error {
